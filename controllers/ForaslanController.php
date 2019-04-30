44a53934ffc1f25b08a600cc6c46bdbd77b9c12a
Light <lightstyle11@mail.ru>
2019-04-30 11:03:33 +0300

    add recalc for page-schet

---
+++ controllers/ForaslanController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lght
+ * Date: 29.12.2018
+ * Time: 15:11
+ */
+
+namespace app\controllers;
+
+
+class ForaslanController
+{
+
+}
---
+++ views/foraslan/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lght
+ * Date: 29.12.2018
+ * Time: 15:13
+ */
